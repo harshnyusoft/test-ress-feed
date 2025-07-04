3363b9823cbc7fd8ebd92a37d24a40f97766af07
harsh.nyusoft <harshgajera.nyusoft@gmail.com>
2025-07-04 18:17:51 +0530

    solve isue

--- src/pages/api/get_rss_feed.js
+++ src/pages/api/get_rss_feed.js
@@ -1,16 +1,8 @@
-// @ts-nocheck
-import { parseStringPromise } from 'xml2js';
-
 const fetchWithHeaders = async (url) => {
-	const response = await fetch("https://www.justice.gov/news/rss", {
+	const response = await fetch(url, {
 		headers: {
 			"User-Agent": "Mozilla/5.0 (compatible; DemoNextApp/1.0; +https://test-ress-feed.vercel.app/)"
 		}
-
-				// headers: {
-		// 	"User-Agent": "Mozilla/5.0 (compatible; DemoNextApp/1.0; +https://taxworkoutgroup.com/)"
-		// }
-
 	});
 
 	if (!response.ok) {
@@ -27,26 +19,3 @@ const fetchWithHeaders = async (url) => {
 	const json = await parseStringPromise(xml, { explicitArray: false });
 	return json?.rss?.channel || json?.feed || json;
 };
-
-
-
-export default async function handler(req, res) {
-	const { url } = req.query;
-
-	if (!url || typeof url !== 'string') {
-		return res.status(400).json({ error: 'URL is required and must be a string' });
-	}
-
-	try {
-		const json = await fetchWithHeaders(url);
-		res.status(200).json(json);
-	} catch (err) {
-		console.error('Error:', err);
-		res.status(500).json({
-			error: err.message || 'Failed to fetch or parse RSS feed',
-			err: err,
-			stack: err.stack,
-			success: false
-		});
-	}
-}
